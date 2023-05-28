2b99ce8cd97bc29af19bca0b7b940f394f9670b9
Jaseel Mohammed <119488337+Adam0265@users.noreply.github.com>
2023-05-28 20:53:12 +0530

    Add files via upload

---
+++ prime_number_for_a_given_interval.py
@@ -0,0 +1,14 @@
+"""
+Python program to print all Prime numbers in an given interval
+"""
+
+def all_prime(start,end):
+    for i in range(start,end +1):
+        if i > 1:
+            for j in range (2,i):
+                if (i % j)==0:
+                    break
+            else:
+                print (i, end= " ")
+
+all_prime(2,56)
